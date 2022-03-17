828f309ac105871c17a9bbf45b8fe98c2a72d0e6
Dmitry <99485278+DmitryIldyukov@users.noreply.github.com>
2022-03-17 19:24:09 +0300

    Add files via upload

---
+++ SQLHW/Repositories/IStudentRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLHW.Models;
+
+namespace SQLHW.Repositories
+{
+    interface IStudentRepository
+    {
+        void Add( Student student );
+        Student GetById( int id );
+        List<Student> GetAll();
+    }
+}
